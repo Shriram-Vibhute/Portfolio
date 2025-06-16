052c82491f2de584f2b28cad647e2f5088c7201e
DEXTER MORGAN <shriramvibhute832002@gmail.com>
2025-06-16 14:21:17 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <HeroSection badge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,14 @@ export default function Home() {
       <HeroSection
         badgeText="Full Stack Developer"
         greeting="Hello, I'm"
-        namePart1="Ramkrishna"
-        namePart2="Swarnkar"
+        namePart1="Shriram"
+        namePart2="Vibhute"
         namePart1Color="text-pink-500"
         namePart2Color="text-yellow-500"
         availabilityText="Available for new projects"
         introParagraph="I'm a Full Stack web developer and Open Source Contributor, with a focus on JavaScript, React, and UI/UX design. Enthusiastic about Three.js, driven by a keen eye for design."
         profileImageUrl="https://placehold.co/288x288.png"
-        profileImageAlt="Ramkrishna Swarnkar Profile Picture"
+        profileImageAlt="Shriram Vibhute Profile Picture"
       />
       <FloatingDockDemo />
     </main>
