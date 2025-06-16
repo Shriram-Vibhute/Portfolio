25984022457c459c68cade58824290d79e5ef56b
DEXTER MORGAN <shriramvibhute832002@gmail.com>
2025-06-16 18:23:28 +0000

    Set the font style of entire website to open snas

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ export default {
     extend: {
       fontFamily: {
         body: ['Open Sans', 'sans-serif'],
-        headline: ['Montserrat', 'sans-serif'],
+        headline: ['Open Sans', 'sans-serif'],
         code: ['Open Sans', 'sans-serif'],
       },
       colors: {
