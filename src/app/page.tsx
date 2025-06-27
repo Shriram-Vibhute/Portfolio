0f56396e8472035912073a003464546972986cf2
DEXTER MORGAN <shriramvibhute832002@gmail.com>
2025-06-27 18:07:09 +0000

    Remove the navbar completly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,10 @@ import { Experience } from "@/components/sections/experience";
 import { Hero } from "@/components/sections/hero";
 import { FadeIn } from "@/components/fade-in";
 import { Footer } from "@/components/footer";
-import { Header } from "@/components/header";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-svh">
-      <Header />
       <main className="flex-grow">
         <FadeIn>
           <Hero />
