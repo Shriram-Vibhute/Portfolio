2b5ce9fb3e98b91be8621f18494400ff61a6b31a
DEXTER MORGAN <shriramvibhute832002@gmail.com>
2025-06-28 12:03:34 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <div class="w-full

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { Skills } from "@/components/sections/skills";
 import { Projects } from "@/components/sections/projects";
 import { Experience } from "@/components/sections/experience";
 import { Education } from "@/components/sections/education";
-import { Contact } from "@/components/sections/contact";
 
 export default function Home() {
   return (
@@ -28,9 +27,6 @@ export default function Home() {
         </FadeIn>
         <FadeIn>
           <Education />
-        </FadeIn>
-        <FadeIn>
-          <Contact />
         </FadeIn>
       </main>
     </div>
