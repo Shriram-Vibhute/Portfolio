f87248ae78f2bcaf24fb318f70082517d6338018
DEXTER MORGAN <shriramvibhute832002@gmail.com>
2025-06-28 12:28:59 +0000

    use my profile as website favicon img

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { Toaster } from '@/components/ui/toaster';
 export const metadata: Metadata = {
   title: 'MirrorFolio | Personal Portfolio',
   description: 'A personal portfolio website built with Next.js and Firebase Studio.',
+  icons: {
+    icon: 'https://drive.google.com/uc?export=view&id=1ya67wIF3eFrA3hlYwfj3D4VI4yFqSEPf',
+  },
 };
 
 export default function RootLayout({
